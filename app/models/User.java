9b95fc5760824edd83406a05ff8f4e9d813c1b7a
EminaMuratovic <emina_peace@hotmail.com>
2015-03-02 18:53:54 +0100

    product i hash passworda

--- app/models/User.java
+++ app/models/User.java
@@ -50,7 +50,7 @@ public class User extends Model {
 	 * @return true or false(if the user is registered)
 	 */
 	public static boolean create(String username, String password) {
-		if(existsUsername(username))
+		if (existsUsername(username))
 			return false;
 		new User(username, password).save();
 		return true;
@@ -83,30 +83,48 @@ public class User extends Model {
 	 * @param passwordToHash Password that needs to be hashed
 	 * @return Hashed password
 	 */
-	public static String hashPw(String passwordToHash){
-		String generatedPassword=null;
+	public static String hashPw(String passwordToHash) {
+		String generatedPassword = null;
 		try {
-            // Create MessageDigest instance for MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            //Add password bytes to digest
-            md.update(passwordToHash.getBytes());
-            //Get the hash's bytes
-            byte[] bytes = md.digest();
-            //This bytes[] has bytes in decimal format;
-            //Convert it to hexadecimal format
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++)
-            {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            //Get complete hashed password in hex format
-            generatedPassword = sb.toString();
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            e.printStackTrace();
-        }
+			// Create MessageDigest instance for MD5
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			// Add password bytes to digest
+			md.update(passwordToHash.getBytes());
+			// Get the hash's bytes
+			byte[] bytes = md.digest();
+			// This bytes[] has bytes in decimal format;
+			// Convert it to hexadecimal format
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < bytes.length; i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
+						.substring(1));
+			}
+			// Get complete hashed password in hex format
+			generatedPassword = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 		return generatedPassword;
+	}
+	
+	/**
+	 * Checking for user's usename and password
+	 * @param username String username of the user
+	 * @param password String Password of the user
+	 * @return Messages depending on whether the login was successful or not
+	 */
+	public static String checkLogin(String username, String password) {
+		String hashedPw = hashPw(password);
+
+		if (find.where().eq("username", username).findList().isEmpty()) {
+			return "Wrong username, try again.";
+		} else {
+			User foundUser = find.where().eq("username", username).findUnique();
+			if (foundUser.password.equals(hashedPw)) {
+				return "Hi " + username + "! Welcome to bitBay!";
+			}
+		}
+		return "Wrong password, try again.";
 	}
 	
 	
