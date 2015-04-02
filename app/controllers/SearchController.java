319233f8694b0f6f312f6b07941e651cd1eb22e4
Mustafa Ademovic <mustafa.ademovic93@gmail.com>
2015-04-02 17:49:37 +0200

    Finished Advanced Search

--- app/controllers/SearchController.java
+++ app/controllers/SearchController.java
@@ -1,12 +1,14 @@
 package controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-import models.FAQ;
-import models.Product;
-import models.User;
-import models.Product;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+
 import models.*;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.*;
 import views.*;
 import views.html.*;
@@ -30,4 +32,39 @@ public class SearchController extends Controller{
 		
 
 	}
+	public static Result advancedSearch() {
+		
+		String email = session().get("email");
+			return ok(advancedsearch.render(email,Category.list(),FAQ.all()));
+		
+
+	}
+	
+	public static Result advancedSearchpage() {
+		DynamicForm form = Form.form().bindFromRequest();
+		Category category = Category.findCategory.where().eq(form.data().get("category"), "name").findUnique();
+		String productName = form.data().get("name");
+		String description = form.data().get("description");
+		int minValue = Integer.parseInt(form.data().get("minPrice"));
+		int maxValue = Integer.parseInt(form.data().get("maxPrice"));
+
+		List<Product> products = Product.find.where()
+				.ilike("category_id", String.valueOf(category.id)).findList();
+		
+		if(productName != null) {
+		products=products.stream().filter(product -> product.name.contains(productName)).collect(Collectors.toList());
+		}
+		
+		if(description != null) {
+			products=products.stream().filter(product -> product.description.contains(description)).collect(Collectors.toList());
+		}
+		if((minValue != 0) && (maxValue != 0) && (minValue < maxValue)) {
+			products=products.stream().filter(product -> ((product.price > minValue) && (product.price > maxValue))).collect(Collectors.toList());
+		}
+		String email = session().get("email");
+			return ok(advancedsearchpage.render(email, products, FAQ.all()));
+	
+
+	}
+	
 }
