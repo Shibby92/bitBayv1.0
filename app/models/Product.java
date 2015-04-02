5914e03619081fc4bb7a4d76ade4e3f67d5350bb
Shibby92 <haris_000@hotmail.com>
2015-04-02 11:13:25 +0200

    nista

--- app/models/Product.java
+++ app/models/Product.java
@@ -1,5 +1,3 @@
-
-
 package models;
 
 import java.io.File;
@@ -17,6 +15,7 @@ import play.db.ebean.Model;
 
 /**
  * Creates product/add
+ * 
  * @author eminamuratovic
  *
  */
@@ -30,22 +29,20 @@ public class Product extends Model {
 	@MaxLength(20)
 	public String name;
 
-	
 	public int category_id;
 
 	@ManyToOne
 	public Cart cart;
-	
+
 	@ManyToOne
 	public User owner;
 
 	public Date created;
 
-	
 	public int quantity;
 
 	@Required
-	@Column(scale = 2) 
+	@Column(scale = 2)
 	public double price;
 
 	@Required
@@ -53,31 +50,41 @@ public class Product extends Model {
 	@MaxLength(144)
 	public String description;
 
-	
 	public String image_url;
-	
+
 	@ManyToOne
 	public Orders order;
-	
+
 	public boolean sold;
 	public static List<String> image_urls;
 
 	public static Finder<Integer, Product> find = new Finder<Integer, Product>(
 			Integer.class, Product.class);
-	static Finder<String,Category> findCategory= new Finder<String,Category>(String.class,Category.class);
-	
+	static Finder<String, Category> findCategory = new Finder<String, Category>(
+			String.class, Category.class);
+
 	/**
 	 * creates a product
-	 * @param name String name of the product
-	 * @param category_id int id of the category
-	 * @param owner_id int id of the seller
-	 * @param created Date date added
-	 * @param quantity int quantity of the product
-	 * @param price double price of the product
-	 * @param description String description of the product
-	 * @param image_url String url of the image of the product
+	 * 
+	 * @param name
+	 *            String name of the product
+	 * @param category_id
+	 *            int id of the category
+	 * @param owner_id
+	 *            int id of the seller
+	 * @param created
+	 *            Date date added
+	 * @param quantity
+	 *            int quantity of the product
+	 * @param price
+	 *            double price of the product
+	 * @param description
+	 *            String description of the product
+	 * @param image_url
+	 *            String url of the image of the product
 	 */
-	public Product(String name, int category_id, User owner, Date created, int quantity, double price, String description, String image_url) {
+	public Product(String name, int category_id, User owner, Date created,
+			int quantity, double price, String description, String image_url) {
 		this.name = name;
 		this.category_id = category_id;
 		this.owner = owner;
@@ -86,67 +93,88 @@ public class Product extends Model {
 		this.price = price;
 		this.description = description;
 		this.image_url = image_url;
-		this.sold=false;
+		this.sold = false;
 	}
-	
+
 	/**
 	 * creates a product with all of the parameters
-	 * @param name String name of the product
-	 * @param category_id int id of the category
-	 * @param owner_id int id of the seller
-	 * @param created Date date added
-	 * @param quantity int quantity of the product
-	 * @param price double price of the product
-	 * @param description String description of the product
-	 * @param image_url String url of the image of the product
+	 * 
+	 * @param name
+	 *            String name of the product
+	 * @param category_id
+	 *            int id of the category
+	 * @param owner_id
+	 *            int id of the seller
+	 * @param created
+	 *            Date date added
+	 * @param quantity
+	 *            int quantity of the product
+	 * @param price
+	 *            double price of the product
+	 * @param description
+	 *            String description of the product
+	 * @param image_url
+	 *            String url of the image of the product
 	 */
-	public static void create(String name, int category_id, User owner, Date created, int quantity, double price, String description, String image_url) {
-		new Product(name, category_id, owner, created, quantity, price, description, image_url).save();
+	public static void create(String name, int category_id, User owner,
+			Date created, int quantity, double price, String description,
+			String image_url) {
+		new Product(name, category_id, owner, created, quantity, price,
+				description, image_url).save();
 	}
 
 	// Constructor for "required" attributes
-	public Product(String name, double price, String description,int id, String image_url) {
+	public Product(String name, double price, String description, int id,
+			String image_url) {
 		this.name = name;
 		this.price = price;
 		this.description = description;
-		this.category_id=id;
-		this.image_url=image_url;
-		this.sold=false;
+		this.category_id = id;
+		this.image_url = image_url;
+		this.sold = false;
 	}
-	public static void create(String name,  double price, String description,int id, String image_url) {
-		new Product(name,  price, description,id,image_url).save();
+
+	public static void create(String name, double price, String description,
+			int id, String image_url) {
+		new Product(name, price, description, id, image_url).save();
 	}
-	
-	public Product(String name, double price, User owner, String description,int id, String image_url) {
-		this.name = name;
-		this.price = price;
-		this.owner = owner;
-		this.description = description;
-		this.category_id=id;
-		this.image_url=image_url;
-		this.sold=false;
-	}
-	
-	public Product(String name, double price, User owner, String description,int id, List<String> image_urls) {
+
+	public Product(String name, double price, User owner, String description,
+			int id, String image_url) {
 		this.name = name;
 		this.price = price;
 		this.owner = owner;
 		this.description = description;
-		this.category_id=id;
-		this.image_urls=image_urls;
+		this.category_id = id;
+		this.image_url = image_url;
+		this.sold = false;
 	}
-	
-	public static void create(String name,  double price, User owner, String description,int id, List<String> image_urls) {
-		new Product(name,  price, owner, description,id,image_urls).save();
+
+	public Product(String name, double price, User owner, String description,
+			int id, List<String> image_urls) {
+		this.name = name;
+		this.price = price;
+		this.owner = owner;
+		this.description = description;
+		this.category_id = id;
+		this.image_urls = image_urls;
 	}
-	
-	public static void create(String name,  double price, User owner, String description,int id, String image_url) {
-		new Product(name,  price, owner, description,id,image_url).save();
+
+	public static void create(String name, double price, User owner,
+			String description, int id, List<String> image_urls) {
+		new Product(name, price, owner, description, id, image_urls).save();
+	}
+
+	public static void create(String name, double price, User owner,
+			String description, int id, String image_url) {
+		new Product(name, price, owner, description, id, image_url).save();
 	}
 
 	/**
 	 * finds a product by his id
-	 * @param id int id of the product
+	 * 
+	 * @param id
+	 *            int id of the product
 	 * @return product
 	 */
 	public static Product find(int id) {
@@ -169,17 +197,18 @@ public class Product extends Model {
 		this.description = description;
 	}
 
-	public static List<Product> productList(){
+	public static List<Product> productList() {
 		return find.all();
 	}
 
 	public static List<Product> listByCategory(String category) {
-		int id=findCategory.where().eq("name",category).findUnique().id;
-		return find.where().eq("category_id",id).findList();
-		
+		int id = findCategory.where().eq("name", category).findUnique().id;
+		return find.where().eq("category_id", id).findList();
+
 	}
 
-	//method which will find id of the product and delete it, else will throw exception,method is used in ProductApplication
+	// method which will find id of the product and delete it, else will throw
+	// exception,method is used in ProductApplication
 	public static void delete(int id) {
 		if (find.byId(id) == null) {
 			throw new IllegalArgumentException("Produkt ne postoji");
@@ -188,29 +217,26 @@ public class Product extends Model {
 			temp.delete();
 		}
 	}
+
 	public static void update(Product product) {
-		Logger.info(""+product.name);
+		Logger.info("" + product.name);
 		product.save();
 	}
-	
+
 	public static List<Product> myProducts(int id) {
-		
+
 		List<Product> pp = find.where("owner_id = " + id).findList();
 		return pp;
 	}
-	
+
 	public static void deleteImage(Product p) {
-		File f = new File("./public/" + p.image_url); 
+		File f = new File("./public/" + p.image_url);
 		boolean b = f.delete();
 
 	}
-	
+
 	public static List<String> allImages() {
 		return image_urls;
 	}
-
-	
-	
- 
 
 }
