99c98012013e9eec277a29f30fc163165606b92a
EminaMuratovic <emina_peace@hotmail.com>
2015-04-03 11:35:36 +0200

    image

--- app/models/Image.java
+++ app/models/Image.java
@@ -9,7 +9,14 @@ import play.db.ebean.Model;
 @Entity
 public class Image extends Model {
 	
-	public List<String> image_url;
-	public int product_id;
+	public static List<String> image_url;
+	public static int product_id;
+	
+	public Image(List<String> image_url, int product_id){
+		this.image_url = image_url;
+		this.product_id = product_id;
+	}
+	
+	
 
 }
