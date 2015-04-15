06b59abcff3b23612b38a798de53e54a3902f352
Shibby92 <haris_000@hotmail.com>
2015-04-15 19:04:20 +0200

    Sredjene relacije, korisnik napokon moze kupiti proizvod

--- app/models/OrderDetails.java
+++ app/models/OrderDetails.java
@@ -1,5 +1,6 @@
 package models;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 
@@ -7,13 +8,10 @@ import play.db.ebean.Model;
 
 @Entity
 @Embeddable
-public class productList_order extends Model{
-
+public class OrderDetails extends Model{
 	
 	public int order_id;
 	
 	public int product_id;
-	
-	
 
 }
