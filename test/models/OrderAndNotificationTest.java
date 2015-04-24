23c3bf82b0f91b799581784d33099964ae24f8c8
Shibby92 <haris_000@hotmail.com>
2015-04-24 13:44:00 +0200

    Notification tests

--- test/models/OrderAndNotificationTest.java
+++ test/models/OrderAndNotificationTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import play.test.WithApplication;
 
 
-public class OrderTest extends WithApplication {
+public class OrderAndNotificationTest extends WithApplication {
 	@Before
 	public void setUp() {
 		fakeApplication(inMemoryDatabase());
@@ -25,21 +25,24 @@ public class OrderTest extends WithApplication {
 	assertEquals(o.productList.get(0).price,24.0,Math.abs(24.0-o.productList.get(0).price));
 	assertEquals(o.productList.get(0).description,"description");
 	assertEquals(o.productList.get(0).quantity,1);
+	User u=new User("buyer@mail.com","123123");
 	o.buyer= new User("buyer@mail.com","123123");
-	o.notification=true;
 	o.price=999.0;
 	o.token="TOKEN";
-	o.seller= new User("seller@mail.com","123123");
 	o.pQ.add(new ProductQuantity(1,1));
 	assertEquals(o.buyer.email,"buyer@mail.com");
 	assertEquals(o.buyer.password,"123123");
-	assertEquals(o.notification,true);
 	assertEquals(o.price,999.0,Math.abs(999.0-o.price));
 	assertEquals(o.token,"TOKEN");
-	assertEquals(o.seller.email,"seller@mail.com");
-	assertEquals(o.seller.password,"123123");
 	assertEquals(o.pQ.get(0).productId,1);
 	assertEquals(o.pQ.get(0).quantity,1);
-	
+	Notification n=new Notification (u,o);
+	n.save();
+	Notification.finder.byId(1);
+	assertNotNull(n);
+	assertEquals(n.id,1);
+	assertEquals(n.isUnchecked,true);
+	assertEquals(n.orderId,1);
+	assertEquals(n.seller,u);	
 	}
 }
